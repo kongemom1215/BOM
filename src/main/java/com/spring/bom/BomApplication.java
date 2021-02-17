426bb91376cac56548c47f7e18291f47d394fb5a
mrgudigus <mrgudigus@gmail.com>
2021-02-17 14:57:37 +0900

    jungchurl commit1

---
+++ src/main/java/com/spring/bom/BomApplication.java
@@ -0,0 +1,13 @@
+package com.spring.bom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/bom/BomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.bom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
